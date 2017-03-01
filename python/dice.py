632ace9e40f33ce9c323cf0d4498a6ee42364ea9
Ian <madgoat@gmail.com>
2017-02-28 21:41:10 -0500

    First program going up

---
+++ python/dice.py
@@ -0,0 +1,14 @@
+import random
+class Die(object):
+  def __init__(self, sides):
+    self.sides = sides
+  def roll(self):
+    return random.randint(1, self.sides)
+
+d = Die(6)
+d1 = Die(6)
+d20 = Die(20)
+print "First Die   : ", d.roll()
+print "Second Die  : ", d1.roll()
+print "Your D20 is : ", d20.roll()
+
